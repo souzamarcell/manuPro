39160ca64902059b0ff061bb05232d96de2a33cf
Marcell Souza <marcell.ramos@gmail.com>
2025-07-30 16:21:33 -0400

    feat: start 1.0.4

--- js/script.js
+++ js/script.js
@@ -41,15 +41,61 @@ function deleteItem(index) {
 
 function insertItem(item, index) {
   let tr = document.createElement('tr')
-  const partes = item.data.split('/') // ["29", "07", "2025"]
-  const dataFormatada = `${partes[0]}/${partes[1]}/${partes[2].slice(-2)}` // "29/07/25"
+
+  const partes = item.data.split('/')
+  const dataFormatada = `${partes[0]}/${partes[1]}/${partes[2].slice(-2)}`
+
+  const nome = item.nome.trim().toLowerCase()
+  let estiloLinha = ''
+
+  switch (nome) {
+    case 'walter':
+      estiloLinha = 'background-color: #f8d7da;' // vermelho claro
+      break
+    case 'cleber':
+      estiloLinha = 'background-color: #cce5ff;' // azul claro
+      break
+    case 'getulio':
+      estiloLinha = 'background-color: #d4edda;' // verde claro
+      break
+    case 'braga':
+      estiloLinha = 'background-color: #ffe5b4;' // abóbora claro
+      break
+    case 'emerson':
+      estiloLinha = 'background-color: #e2e3e5;' // cinza claro
+      break
+    case 'miro':
+      estiloLinha = 'background-color: #000000; color: white;' // preto com texto branco
+      break
+    case 'pepeu':
+      estiloLinha = 'background-color: #e6ccff;' // roxo claro
+      break
+    case 'josian':
+      estiloLinha = 'background-color: #fff3cd;' // amarelo claro
+      break
+  }
+
+  tr.setAttribute('style', estiloLinha)
+
+  // Estilos por função para texto
+  const funcao = item.funcao.trim().toLowerCase()
+  let funcaoStyle = ''
+
+  if (funcao === 'eletricista') {
+    funcaoStyle = 'style="color: blue; font-weight: bold;"'
+  } else if (funcao === 'mecânico') {
+    funcaoStyle = 'style="color: red; font-weight: bold;"'
+  } else if (funcao === 'utilidades') {
+    funcaoStyle = 'style="color: green; font-weight: bold;"'
+  }
+
   tr.innerHTML = `
     <td class="dataHora">
       ${dataFormatada}<br>
       ${item.hora}
     </td>
     <td class="nomes">${item.nome}</td>
-    <td class="funcao">${item.funcao}</td>
+    <td class="funcao" ${funcaoStyle}>${item.funcao}</td>
     <td>${item.salario}</td>
     <td class="acaoDuplo">
       <button onclick="editItem(${index})">
@@ -62,6 +108,7 @@ function insertItem(item, index) {
       </button>
     </td>
   `
+
   tbody.appendChild(tr)
 }
 
@@ -113,5 +160,33 @@ function loadItens() {
 
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+const nomeParaFuncao = {
+  Walter: 'Mecânico',
+  Cleber: 'Mecânico',
+  Getulio: 'Mecânico',
+  Braga: 'Eletricista',
+  Emerson: 'Eletricista',
+  Miro: 'Eletricista',
+  Pepeu: 'Utilidades',
+  Josian: 'Utilidades',
+}
+
+const nomeSelect = document.getElementById('m-nome')
+const funcaoSelect = document.getElementById('m-funcao')
+
+nomeSelect.addEventListener('change', () => {
+  const nomeSelecionado = nomeSelect.value
+  const funcaoCorrespondente = nomeParaFuncao[nomeSelecionado]
+
+  if (funcaoCorrespondente) {
+    for (let option of funcaoSelect.options) {
+      if (option.value === funcaoCorrespondente) {
+        option.selected = true
+        break
+      }
+    }
+  }
+})
 
 loadItens()
